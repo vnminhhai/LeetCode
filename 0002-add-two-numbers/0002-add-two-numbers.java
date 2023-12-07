e6fd6c2568a78bfe825d9a8e10e67298d1772bd6
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-08 00:53:17 +0700

    Time: 1 ms (100.00%), Space: 43 MB (71.46%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,17 +12,18 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode l3 = new ListNode((l1.val+l2.val)%10);
         ListNode l4=l3;
-        boolean c=((l1.val+l2.val)>9) ? true : false;
+        boolean c=(l1.val+l2.val)>9;
         while (l1.next!=null || l2.next!=null || c) {
-            l1=(l1.next==null) ? new ListNode(0) : l1.next; l2=(l2.next==null) ? new ListNode(0) : l2.next;
+            l1=(l1.next==null) ? new ListNode(0) : l1.next;
+            l2=(l2.next==null) ? new ListNode(0) : l2.next;
             if (c) {
                 l3.next= new ListNode((l1.val+l2.val+1)%10);
                 l3=l3.next;
-                c=((l1.val+l2.val+1)>9) ? true : false;
+                c=(l1.val+l2.val+1)>9;
             } else {
                 l3.next= new ListNode((l1.val+l2.val)%10);
                 l3=l3.next;
-                c=((l1.val+l2.val)>9) ? true : false;
+                c=(l1.val+l2.val)>9;
             }
         }
         return l4;
