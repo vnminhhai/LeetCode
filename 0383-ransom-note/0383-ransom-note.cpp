6a8471c44f877289d2fbfea583cc58c979e5ef95
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-11 11:49:49 +0700

    Time: 7 ms (97.07%), Space: 9 MB (85.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> mag;
+        int mag[26];
+                
+        for (int i=0; i<26; i++) {
+            mag[i]=0;
+        }
         for (int i=0; i<magazine.length(); ++i) {
-            if (mag.count(magazine[i])==0)
-                mag.insert(pair<char,int>(magazine[i],1));
-            else mag.at(magazine[i])++;
+            mag[magazine[i]-'a']++;
         }
         for (int i=0; i<ransomNote.length(); ++i) {
-            if (mag.count(ransomNote[i])==0) return false;
-            else {
-                mag.at(ransomNote[i])--;
-                if (mag.at(ransomNote[i])==0) mag.erase(ransomNote[i]);
-            }
+            if (mag[ransomNote[i]-'a']--<1) return false;
         }
-        
         return true;
     }
 };
