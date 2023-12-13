f0c5cd2886f5181c997410940efac6df0a1e0d6f
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-13 17:53:12 +0700

    Time: 17 ms (54.47%), Space: 13.4 MB (12.89%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int ans = 0;
         int m = mat.size();
         int n = mat[0].size();
+        vector<int> rowCount(m, 0);
+        vector<int> colCount(n, 0);
         
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
-                if (mat[row][col] == 0) {
-                    continue;
+                if (mat[row][col] == 1) {
+                    rowCount[row]++;
+                    colCount[col]++;
                 }
-                
-                bool good = true;
-                for (int r = 0; r < m; r++) {
-                    if (r != row && mat[r][col] == 1) {
-                        good = false;
-                        break;
+            }
+        }
+        
+        int ans = 0;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 1) {
+                    if (rowCount[row] == 1 && colCount[col] == 1) {
+                        ans++;
                     }
-                }
-                
-                for (int c = 0; c < n; c++) {
-                    if (c != col && mat[row][c] == 1) {
-                        good = false;
-                        break;
-                    }
-                }
-                
-                if (good) {
-                    ans++;
                 }
             }
         }
