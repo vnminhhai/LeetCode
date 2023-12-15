a645d81a978c1da77d51019a8724fdb946dd6421
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-15 20:47:32 +0700

    Time: 0 ms (100.00%), Space: 41 MB (26.99%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,22 +14,16 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root.left==null&&root.right==null) return true;
-        if (root.left==null||root.right==null) return false;
-        ArrayList<TreeNode> t1 =new ArrayList<>(), t2 =new ArrayList<>();
-        t1.add(root.left);
-        t2.add(root.right);
-        while (!t1.isEmpty()&&!t2.isEmpty()) {
-            TreeNode n1 = t1.remove(0), n2=t2.remove(0);
-            if (n1==null&&n2==null) continue;
-            if (n1==null||n2==null) return false;
-            if (n1.val!=n2.val) return false;
-            t1.add(n1.left);
-            t1.add(n1.right);
-            t2.add(n2.right);
-            t2.add(n2.left);
-        }
-        return t1.isEmpty()&&t2.isEmpty();
+    static boolean mir( TreeNode t1 , TreeNode t2 )
+    {
+        if( t1==null && t2== null ) return true;
+        else if( t1==null || t2==null ) return false;
+
+        return ( t1.val==t2.val ) && mir( t1.right , t2.left ) && mir( t1.left , t2.right );
+    }
+    public boolean isSymmetric(TreeNode root) 
+    {
+        return mir( root , root );
+    
     }
 }
