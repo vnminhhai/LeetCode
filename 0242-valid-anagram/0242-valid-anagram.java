8209685d9624a955ae93e0cb3ea2024c5d9fd1bb
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-16 18:40:51 +0700

    Time: 2 ms (98.39%), Space: 44.1 MB (12.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,8 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] ss=s.toCharArray(), ts=t.toCharArray();
-        Arrays.sort(ts);
-        Arrays.sort(ss);
-        return (String.valueOf(ss).equals(String.valueOf(ts)));        
-    }
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int map[]=new int[26];
+        for(char x : s.toCharArray()){
+            map[x-'a']++;
+        }
+        for(char x : t.toCharArray()){
+            map[x-'a']--;
+        }
+        for(int i:map){
+            if (i!=0){
+                return false;
+            }
+        }
+        return true;
+        }
 }
