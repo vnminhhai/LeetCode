9e2a02d3a5b31dc2b6248d7ce5341f1f2fa03553
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-20 22:51:55 +0700

    Time: 3 ms (97.18%), Space: 44.2 MB (8.35%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,37 +1,37 @@
 class Solution {
     public String intToRoman(int num) {
-        String res="";
+        StringBuilder res=new StringBuilder();
         switch ((int) Math.log10(num)) {
             case 3:
                 int c=num/1000;
                 num=num%1000;
                 while (c-->0) {
-                    res+="M";
+                    res.append("M");
                 }
             case 2:
                 c=num/100;
                 num=num%100;
-                if (c==4||c==9) { res+= (c==4) ? "CD" : "CM"; c=0; }
-                else if (c>=5) { res+="D"; c-=5; }
+                if (c==4||c==9) { res.append((c==4) ? "CD" : "CM"); c=0; }
+                else if (c>=5) { res.append("D"); c-=5; }
                 while (c-->0) {
-                    res+="C";
+                    res.append("C");
                 }
             case 1:
                 c=num/10;
                 num=num%10;
-                if (c==4||c==9) { res+= (c==4) ? "XL" : "XC"; c=0; }
-                else if (c>=5) { res+="L"; c-=5; }
+                if (c==4||c==9) { res.append( (c==4) ? "XL" : "XC"); c=0; }
+                else if (c>=5) { res.append("L"); c-=5; }
                 while (c-->0) {
-                    res+="X";
+                    res.append("X");
                 }
             default:
                 c=num%10;
-                if (c==4||c==9) { res+= (c==4) ? "IV" : "IX"; c=0; }
-                else if (c>=5) { res+="V"; c-=5; }
+                if (c==4||c==9) { res.append( (c==4) ? "IV" : "IX"); c=0; }
+                else if (c>=5) { res.append("V"); c-=5; }
                 while (c-->0) {
-                    res+="I";
+                    res.append("I");
                 }
         }
-        return res;
+        return res.toString();
     }
 }
