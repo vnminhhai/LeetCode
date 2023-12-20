cc7300645db7aa251ce5f20bc8b59615a9b3bbda
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-20 20:54:34 +0700

    Time: 1 ms (100.00%), Space: 44.2 MB (5.53%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int m1=prices[0], m2=100;
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i]<m1) {
+                m2=m1;
+                m1=prices[i];
+            }
+            else if (prices[i]<m2) {
+                m2=prices[i];
+            }
+        }
+        return money-m1-m2>=0 ? money-m1-m2 : money;
+    }
+}
