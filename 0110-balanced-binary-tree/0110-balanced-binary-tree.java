32f85df175943b61667ee7f643dc58c88b6b0de9
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2023-12-21 21:34:15 +0700

    Time: 0 ms (100.00%), Space: 44 MB (6.01%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,24 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == null)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
-	}
-    // Create a function to return the “height” of a current subtree using recursion...
-	public int Height(TreeNode root) {
-        // Base case...
-		if (root == null)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root.left);
-        // Height of height subtree...
-		int rightHight = Height(root.right);
-        // In case of left subtree or right subtree unbalanced, return -1...
-		if (leftHeight == -1 || rightHight == -1)  return -1;
-        // If their heights differ by more than ‘1’, return -1...
-        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return Math.max(leftHeight, rightHight) + 1;
+        if (root==null) return true;
+        if (dep(root)==-1) return false;
+        return true;
+    }
+    int dep(TreeNode n) {
+        if (n==null) return 0;
+        int dl=dep(n.left), dr=dep(n.right);
+        if (dl==-1||dr==-1) return -1;
+        if (Math.abs(dl-dr)>1) return -1;
+        return Math.max(dl,dr)+1;
     }
 }
