22b2fb9f07a1cff339b5fecf28ed5a6ca93c460c
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2024-01-06 23:07:17 +0700

    Time: 1 ms (99.99%), Space: 45.8 MB (11.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,13 @@
 class Solution {
-    static boolean[] dp;
+    static boolean res;
     static int len;
     public boolean canJump(int[] nums) {
         len=nums.length;
         if (len==1) return true;
         int min=len-1;
-        dp= new boolean[len];
-        Arrays.fill(dp,false);
         for (int i=len-2; i>=0; i--) {
-            if (nums[i]+i>=min) {dp[i]=true; min=i;}
+            if (nums[i]+i>=min) min=i;
         }
-        return dp[0];
+        return min==0;
     }
 }
