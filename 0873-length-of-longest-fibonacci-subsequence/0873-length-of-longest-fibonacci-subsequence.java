bf361b19d8e369045c7ccba30e70d75c938cb300
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2024-01-10 00:00:09 +0700

    Time: 52 ms (89.86%), Space: 66 MB (5.31%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -1,27 +1,9 @@
 class Solution {
+    static int max, dp[][];
     public int lenLongestFibSubseq(int[] A) {
-        // int n = A.length;
-        // int max = 0;
-        // int[][] dp = new int[n][n];
-        // for (int i = 2; i < n; i++) {
-        //     int l = 0, r = i - 1;
-        // while (l < r) {
-        //         int sum = A[l] + A[r];
-        //         if (sum > A[i]) {
-        //             r--;  
-        //         } else if (sum < A[i]) {
-        //             l++;
-        //         } else {
-        //             dp[r][i] = dp[l][r] + 1;
-        //             max = Math.max(max, dp[r][i]);
-        //             r--;
-        //             l++;
-        //         }
-        //     }
-        // }
-        // return max == 0 ? 0 : max + 2;
-        int len= A.length, max=0;
-        int[][] dp = new int[len][len];
+        int len= A.length;
+        max=0;
+        dp = new int[len][len];
         for (int i=2; i<len; i++) {
             int l=0, r=i-1;
             while (l<r) {
