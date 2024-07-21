204e74162ed932d730b4a18d1b10e3c2ac8dfa78
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2024-07-21 23:39:51 +0700

    Time: 18 ms (16.39%), Space: 51.1 MB (80.4%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -3,18 +3,25 @@ class Solution {
     public List<Integer> grayCode(int n) {
         list= new ArrayList<>();
         list.add(0);
-        int times = (int)(Math.pow(2,n));
+        int times = (int)(pow(2,n));
         for (int i=1; i<times; i++) {
             list.add(list.get(opp(i))^helper(i));
+            // list.add(helper(i));
         }
         return list;
     }
     int helper(int i) {
-        int c =0;
-        while (i>0) {i/=2; c++;}
-        return (int)(Math.pow(2,c-1));
+        return  pow(2,(int)Math.ceil(Math.log(i+1)/Math.log(2))-1);
     }
     int opp(int i) {
         return helper(i)*2-1-i;
+    }
+    int pow (int a, int b)
+    {
+        if (b == 0)     return 1;
+        if (b == 1)     return a;
+        if (b%2==0)  return     pow (a * a, b/2);     //even a=(a^2)^b/2
+        else            return a * pow (a * a, b/2);     //odd  a=a*(a^2)^b/2
+
     }
 }
