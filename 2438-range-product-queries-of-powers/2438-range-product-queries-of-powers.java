fe9c049fa7f6c226868a4ff8adb606474db16917
vnminhhai <142912018+vnminhhai@users.noreply.github.com>
2025-08-11 15:28:54 +0700

    Time: 28 ms (27.96%), Space: 88.6 MB (46.24%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -1,4 +1,6 @@
 class Solution {
+    static List<Integer> bins = new ArrayList<>();
+
     public int[] productQueries(int n, int[][] queries) {
         int[] powers = getPowers(n);
         long[] result = new long[queries.length];
@@ -6,7 +8,7 @@ class Solution {
         for (int i=0; i<queries.length; i++) {
             result[i] = 1;
             for (int j=queries[i][0]; j<=queries[i][1]; j++) {
-                result[i]*= (int) Math.pow(2,powers[j]);
+                result[i]*= powers[j];
                 result[i] %= 1000000007;
             }
         }
@@ -15,12 +17,12 @@ class Solution {
 
     public int[] getPowers(int n) {
         ArrayList<Integer> powers = new ArrayList<>();
-        int currentPower = 0;
+        int currentPower = 1;
         while (n > 0) {
             if (n % 2 == 1) {
                 powers.add(currentPower);
             }
-            currentPower++;
+            currentPower*=2;
             n=n>>>1;
         }
         return powers.stream().mapToInt(i -> i).toArray();
